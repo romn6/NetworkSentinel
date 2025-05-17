ac1ef6babd08a9c49dbef9c064c3ec5bd35aaa7c
romn6 <najeraroman6@gmail.com>
2025-05-16 22:13:01 -0600

    Temporary placeholder

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+* NetworkSnetinel - A network intrusion detection system. This is currently a placeholder for main. 
+* For the moment being, go ahead and visit the README.md file for more information on how to use this project.
+* Currently, This project's main developer is myself romn6.  Hope you are able to follow along and watch this project grow.
+*/
+
+#include <stdio.h>
+
+/*
+* This is the main function of the NetworkSentinel program.
+* It serves as the entry point for the application.
+*/
+int main() {
+    printf("NetworkSentinel started.\n"); 
+    return 0;
+}
